82017472ee1310f19c5bc19fda7095ac280790a1
Andrija <akinovak@gmail.com>
2021-12-15 00:42:38 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import { _Z4facti } from './factorial.wasm';
-import init, { MuxWasm, initThreadPool } from "rln"
-
 const myCrossOriginIsolated = self.crossOriginIsolated;
 if (myCrossOriginIsolated) {
   console.log('isolated')
@@ -8,20 +5,6 @@ if (myCrossOriginIsolated) {
   console.log('not isolated -> something is probably wrong with headers')
 
 }
-
-// const worker = new Worker(new URL('./worker.js', import.meta.url));
-// worker.postMessage({
-//   question:
-//     'The Answer to the Ultimate Question of Life, The Universe, and Everything.',
-// });
-// worker.onmessage = ({ data: { answer } }) => {
-//   console.log(answer);
-// };
-
-// (async () => {
-//   await init();
-//   await initThreadPool(navigator.hardwareConcurrency);
-// })();
 
 if (window.Worker) {
   const worker = new Worker(new URL('./worker.js', import.meta.url));
